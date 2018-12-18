8cfe23a43223248bb14e8d6b5ac38d73e777c7ed
sspinelli <sacha.bba@gmail.com>
2018-12-18 17:52:23 -0300

    Connecting to memcache.

--- tabi/settings.py
+++ tabi/settings.py
@@ -87,16 +87,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tabi.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     },
-#     'info_db': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'info_db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'info_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'info_db.sqlite3'),
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -178,6 +178,55 @@ SCRAPING_URLS = [
 ]
 
 django_heroku.settings(locals())
+
+
+def get_cache():
+    import os
+    try:
+        servers = os.environ['MEMCACHIER_SERVERS']
+        username = os.environ['MEMCACHIER_USERNAME']
+        password = os.environ['MEMCACHIER_PASSWORD']
+
+        return {
+          'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+            # TIMEOUT is not the connection timeout! It's the default expiration
+            # timeout that should be applied to keys! Setting it to `None`
+            # disables expiration.
+            'TIMEOUT': None,
+            'LOCATION': servers,
+            'OPTIONS': {
+                'binary': True,
+                'username': username,
+                'password': password,
+                'behaviors': {
+                    # Enable faster IO
+                    'no_block': True,
+                    'tcp_nodelay': True,
+                    # Keep connection alive
+                    'tcp_keepalive': True,
+                    # Timeout settings
+                    'connect_timeout': 2000, # ms
+                    'send_timeout': 750 * 1000, # us
+                    'receive_timeout': 750 * 1000, # us
+                    '_poll_timeout': 2000, # ms
+                    # Better failover
+                    'ketama': True,
+                    'remove_failed': 1,
+                    'retry_timeout': 2,
+                    'dead_timeout': 30,
+                }
+            }
+          }
+        }
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
 
 
 try:
